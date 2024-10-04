0bef244393cda899e57beebdace4bd1ad3b95b6a
Thyon3 <asnakemengesha80@gmail.com>
2024-10-04 11:00:00 -0400

    feat: add global hooks component for data initialization

---
+++ components/layout/GlobalHooks.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { auth } from "@/firebase/clientApp";
+import { useCommunitySnippets } from "@/hooks/community/useCommunitySnippets";
+import useSavedPosts from "@/hooks/posts/useSavedPosts";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+/**
+ * A headless component that initializes global data based on the user's authentication state.
+ * Bootstraps community subscriptions and saved posts into global state atoms.
+ * @returns null, as this component only performs side effects.
+ */
+const GlobalHooks: React.FC = () => {
+  useCommunitySnippets();
+  const { fetchSavedPosts, setSavedPostState } = useSavedPosts();
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetchSavedPosts();
+    } else {
+      setSavedPostState((prev) => ({
+        ...prev,
+        savedPosts: [],
+        fetched: false,
+      }));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return null;
+};
+
+export default GlobalHooks;

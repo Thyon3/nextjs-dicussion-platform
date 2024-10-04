07dfb2e4e8b9e0ad5342130109fb40b1234955bb
Thyon3 <asnakemengesha80@gmail.com>
2024-10-04 14:00:00 -0400

    feat: add responsive page content layout component

---
+++ components/layout/PageContent.tsx
@@ -0,0 +1,41 @@
+import { Flex } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+type PageContentProps = {
+  children: ReactNode;
+};
+
+/**
+ * A responsive two-column layout component used for page content.
+ * Expects two children: first for main content area (left) and second for sidebar (right).
+ * The sidebar is hidden on smaller screens.
+ * @param children - The content to be displayed in two columns.
+ * @returns A centered flex container with responsive column widths.
+ */
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    <Flex justify="center" p="16px 0px">
+      <Flex width="95%" justify="center" maxWidth="1200px">
+        {/* Left */}
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+        >
+          {/* check if children exist before rendering */}
+          {children && children[0 as keyof typeof children]}
+        </Flex>
+        {/* Right */}
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PageContent;

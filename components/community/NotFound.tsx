bfe450e03085cf2c8271a680443de3771e9de67c
Thyon3 <asnakemengesha80@gmail.com>
2024-10-08 09:00:00 -0400

    feat: add community not found component

---
+++ components/community/NotFound.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Flex, Button, Stack, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+/**
+ * Friendly empty state shown when a community ID is invalid.
+ * @returns Message with links back to home and discovery.
+ */
+const CommunityNotFound: React.FC = () => {
+  return (
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="60vh"
+    >
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        color={{ base: "gray.600", _dark: "gray.400" }}
+      >
+        Sorry, this community does not exist!
+      </Text>
+      <Stack direction="row" gap={4} mt={4}>
+        <Link href="/">
+          <Button mt={4} width="150px">
+            Home
+          </Button>
+        </Link>
+        <Link href="/communities">
+          <Button mt={4} width="150px">
+            All Communities
+          </Button>
+        </Link>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default CommunityNotFound;

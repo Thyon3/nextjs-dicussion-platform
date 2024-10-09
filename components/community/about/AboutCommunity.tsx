702bbc25b817bdad459781ddfdeb37f42d85d30a
Thyon3 <asnakemengesha80@gmail.com>
2024-10-09 11:00:00 -0400

    feat: add community stats component

---
+++ components/community/about/AboutCommunity.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Community } from "@/types/community";
+import { Flex, Text } from "@chakra-ui/react";
+import moment from "moment";
+
+interface AboutCommunityProps {
+  communityData: Community;
+}
+
+/**
+ * Displays community stats for the about card.
+ * @param communityData - Community metadata including member count and createdAt.
+ * @returns Two-column row showing subscribers and creation date.
+ */
+const AboutCommunity: React.FC<AboutCommunityProps> = ({ communityData }) => {
+  return (
+    <Flex width="100%" p={2} fontSize="10pt">
+      <Flex direction="column" flexGrow={1}>
+        <Text fontWeight={700}>Subscribers</Text>
+        <Text>{communityData.numberOfMembers.toLocaleString()}</Text>
+      </Flex>
+      <Flex direction="column" flexGrow={1}>
+        <Text fontWeight={700}>Created</Text>
+        <Text>
+          {communityData.createdAt &&
+            moment(new Date(communityData.createdAt.seconds * 1000)).format(
+              "MMM DD, YYYY"
+            )}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default AboutCommunity;

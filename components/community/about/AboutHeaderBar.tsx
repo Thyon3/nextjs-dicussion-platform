6869ff00c3fd5352a3857e4f0caaf83012c07e60
Thyon3 <asnakemengesha80@gmail.com>
2024-10-09 09:00:00 -0400

    feat: add about header bar component

---
+++ components/community/about/AboutHeaderBar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Flex, Text, Icon } from "@chakra-ui/react";
+import { HiOutlineDotsHorizontal } from "react-icons/hi2";
+
+interface AboutHeaderBarProps {
+  communityName: string;
+}
+
+/**
+ * Header bar for the community about card.
+ * @param communityName - Community id to display.
+ * @returns Banner with title and action icon placeholder.
+ */
+const AboutHeaderBar: React.FC<AboutHeaderBarProps> = ({ communityName }) => {
+  return (
+    <Flex
+      justify="space-between"
+      align="center"
+      bg={{ base: "red.500", _dark: "red.600" }}
+      color="white"
+      p={3}
+      borderRadius="10px 10px 0px 0px"
+    >
+      <Text fontSize="10pt" fontWeight={700}>
+        About {communityName}
+      </Text>
+      <Icon as={HiOutlineDotsHorizontal} />
+    </Flex>
+  );
+};
+
+export default AboutHeaderBar;

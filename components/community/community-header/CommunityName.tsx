695d55e575209a80f5133fa0510b4203452d8cf7
Thyon3 <asnakemengesha80@gmail.com>
2024-10-14 14:00:00 -0400

    feat: add community name component

---
+++ components/community/community-header/CommunityName.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+type CommunityNameProps = {
+  id: string;
+};
+
+/**
+ * Displays the community ID in the header.
+ * @param id - Community slug.
+ * @returns Typography block with the name.
+ */
+const CommunityName: React.FC<CommunityNameProps> = ({ id }) => {
+  return (
+    <Flex direction="column" mr={6}>
+      <Text fontWeight={800} fontSize="16pt">
+        {id}
+      </Text>
+    </Flex>
+  );
+};
+
+export default CommunityName;

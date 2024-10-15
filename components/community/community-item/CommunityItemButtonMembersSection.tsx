e656e9720d7ad822964a25dc8191d553dd0129d0
Thyon3 <asnakemengesha80@gmail.com>
2024-10-15 16:00:00 -0400

    feat: add community item button members section

---
+++ components/community/community-item/CommunityItemButtonMembersSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Community } from "@/types/community";
+import { Stack, Flex, Icon, Button } from "@chakra-ui/react";
+import { BsFillPeopleFill } from "react-icons/bs";
+
+type CommunityItemButtonMembersSectionProps = {
+  community: Community;
+  onJoinOrLeaveCommunity: (community: Community, isJoined: boolean) => void;
+  isJoined: boolean;
+};
+
+/**
+ * Shows member count and a subscribe/unsubscribe button for a community list row.
+ * @param community - Community data including member count.
+ * @param onJoinOrLeaveCommunity - Handler for join/leave clicks.
+ * @param isJoined - Membership flag to toggle variant and label.
+ * @returns Row with count icon and action button.
+ */
+const CommunityItemButtonMembersSection: React.FC<
+  CommunityItemButtonMembersSectionProps
+> = ({ community, onJoinOrLeaveCommunity, isJoined }) => {
+  return (
+    <Stack direction="row" align="center" justifyContent="space-between">
+      <Flex
+        fontSize={18}
+        color={{ base: "gray.500", _dark: "gray.400" }}
+        justify="center"
+        align="center"
+        mr={2}
+      >
+        <Icon as={BsFillPeopleFill} mr={1} />
+        {community.numberOfMembers}
+      </Flex>
+      <Button
+        height="30px"
+        width="130px"
+        fontSize="10pt"
+        variant={isJoined ? "outline" : "solid"}
+        onClick={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          onJoinOrLeaveCommunity(community, isJoined);
+        }}
+      >
+        {isJoined ? "Unsubscribe" : "Subscribe"}
+      </Button>
+    </Stack>
+  );
+};
+
+export default CommunityItemButtonMembersSection;

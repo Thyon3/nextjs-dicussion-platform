a1b503a921ca9c1bf929dc0d06ad65ea08182097
Thyon3 <asnakemengesha80@gmail.com>
2024-10-29 11:00:00 -0400

    feat: add create community index

---
+++ components/modal/create-community/index.ts
@@ -0,0 +1,4 @@
+export { default as CreateCommunityModal } from "./CreateCommunityModal";
+export { default as CommunityNameSection } from "./CommunityNameSection";
+export { default as CommunityTypeOptions } from "./CommunityTypeOptions";
+export { default as CommunityTypeOption } from "./CommunityTypeOption";

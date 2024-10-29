af35c8936718e2f626eca9b9225dbe4dd945c610
Thyon3 <asnakemengesha80@gmail.com>
2024-10-29 09:00:00 -0400

    feat: add community type option component

---
+++ components/modal/create-community/CommunityTypeOption.tsx
@@ -0,0 +1,64 @@
+import React, { FC } from "react";
+import { CheckboxCard, Icon, Flex, VStack } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
+
+type CommunityTypeOptionProps = {
+  name: string;
+  icon: IconType;
+  label: string;
+  description: string;
+  isChecked: boolean;
+  onChange: (value: string) => void;
+};
+
+/**
+ * Single selectable option for community privacy type.
+ * @param name - Value representing type.
+ * @param icon - Icon to display.
+ * @param label - Human-readable label.
+ * @param description - Short description of permissions.
+ * @param isChecked - Whether this option is active.
+ * @param onChange - Callback when option is chosen.
+ * @returns Checkbox-styled card for selection.
+ */
+const CommunityTypeOption: FC<CommunityTypeOptionProps> = ({
+  name,
+  icon,
+  label,
+  description,
+  isChecked,
+  onChange,
+}) => {
+  return (
+    <CheckboxCard.Root
+      value={name}
+      checked={isChecked}
+      onCheckedChange={() => onChange(name)}
+      colorPalette="red"
+      borderRadius="xl"
+      cursor="pointer"
+    >
+      <CheckboxCard.HiddenInput />
+      <CheckboxCard.Control>
+        <CheckboxCard.Content>
+          <Flex align="center" gap={3}>
+            <Icon
+              as={icon}
+              fontSize="24px"
+              color={{ base: "gray.500", _dark: "gray.400" }}
+            />
+            <VStack align="start" gap={0}>
+              <CheckboxCard.Label fontSize="10pt">{label}</CheckboxCard.Label>
+              <CheckboxCard.Description fontSize="8pt">
+                {description}
+              </CheckboxCard.Description>
+            </VStack>
+          </Flex>
+        </CheckboxCard.Content>
+        <CheckboxCard.Indicator />
+      </CheckboxCard.Control>
+    </CheckboxCard.Root>
+  );
+};
+
+export default CommunityTypeOption;

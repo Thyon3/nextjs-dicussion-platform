9753b361c5dcb975f1c91ca92651bd5e1c0d50ae
Thyon3 <asnakemengesha80@gmail.com>
2024-10-31 14:00:00 -0400

    feat: add new post form index

---
+++ components/posts/new-post-form/index.ts
@@ -0,0 +1,3 @@
+export { default as NewPostForm } from "./NewPostForm";
+export { default as BackToCommunityButton } from "./BackToCommunityButton";
+export { default as PostCreateError } from "./PostCreateError";

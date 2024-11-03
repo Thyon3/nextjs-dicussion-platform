c31f0416f734e2f5b8064888b576dd0e5b67b32e
Thyon3 <asnakemengesha80@gmail.com>
2024-11-03 14:00:00 -0500

    feat: add posts index

---
+++ components/posts/index.ts
@@ -0,0 +1,8 @@
+export { default as Posts } from "./Posts";
+export { default as NewPostForm } from "./new-post-form/NewPostForm";
+export { default as PostItem } from "./post-item/PostItem";
+export { default as PostTitle } from "./post-item/PostTitle";
+export { default as PostBody } from "./post-item/PostBody";
+export { default as PostDetails } from "./post-item/PostDetails";
+export { default as PostActions } from "./post-item/PostActions";
+export { default as VoteSection } from "./post-item/VoteSection";

dc67c770e6b59ac6a1ecb42f4c6698c86122ec88
Thyon3 <asnakemengesha80@gmail.com>
2024-11-03 09:00:00 -0500

    feat: add post item index

---
+++ components/posts/post-item/index.ts
@@ -0,0 +1,6 @@
+export { default as PostItem } from "./PostItem";
+export { default as PostTitle } from "./PostTitle";
+export { default as PostBody } from "./PostBody";
+export { default as PostDetails } from "./PostDetails";
+export { default as PostActions } from "./PostActions";
+export { default as VoteSection } from "./VoteSection";

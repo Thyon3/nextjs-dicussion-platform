6ca2a1a5865cc0cb3d0c77801eb6718536c6a68e
Thyon3 <asnakemengesha80@gmail.com>
2024-11-07 14:00:00 -0500

    feat: add posts hooks index

---
+++ hooks/posts/index.ts
@@ -0,0 +1,7 @@
+export { default as useSavedPosts } from "./useSavedPosts";
+export { default as usePostState } from "./usePostState";
+export { default as usePostSelection } from "./usePostSelection";
+export { default as usePostVote } from "./usePostVote";
+export { default as usePostVoteSync } from "./usePostVoteSync";
+export { default as usePostDeletion } from "./usePostDeletion";
+export { default as usePostsFeed } from "./usePostsFeed";

a5e4173b92c8b16a4c7b95ecc5ed0888c1e1eba9
Thyon3 <asnakemengesha80@gmail.com>
2024-11-08 09:00:00 -0500

    feat: add use create post hook

---
+++ hooks/posts/useCreatePost.ts
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { User } from "firebase/auth";
+import useCustomToast from "@/hooks/useCustomToast";
+import { createPost } from "@/lib/posts/createPost";
+import useCommunityState from "../community/useCommunityState";
+import { checkCommunityPermission } from "@/lib/community/communityPermissions";
+
+/**
+ * A custom hook that provides functionality for creating a new post.
+ * It handles permission checks for restricted communities, image uploads, and provides feedback via toasts.
+ * @returns An object containing `handleCreatePost` function, loading state, and error state.
+ */
+const useCreatePost = () => {
+  const router = useRouter();
+  const showToast = useCustomToast();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const { communityStateValue } = useCommunityState();
+
+  const handleCreatePost = async (
+    user: User,
+    communityId: string,
+    communityImageURL: string | undefined,
+    postData: { title: string; body: string },
+    selectedFile?: string
+  ) => {
+    if (!user) return;
+    setLoading(true);
+
+    // Check for restricted community permissions
+    const currentCommunity = communityStateValue.currentCommunity;
+    if (currentCommunity?.id === communityId) {
+      const hasPermission = checkCommunityPermission(
+        currentCommunity,
+        communityStateValue.mySnippets
+      );
+
+      if (!hasPermission) {
+        showToast({
+          title: "Restricted Community",
+          description: "You must be a member to post in this community.",
+          status: "error",
+        });
+        setLoading(false);
+        return;
+      }
+    }
+
+    try {
+      await createPost(
+        user,
+        communityId,
+        communityImageURL,
+        postData,
+        selectedFile
+      );
+
+      router.back();
+      showToast({
+        title: "Post Created",
+        description: "Your post has been created successfully",
+        status: "success",
+      });
+    } catch (error: any) {
+      console.log("handleCreatePost error", error);
+      setError(true);
+      showToast({
+        title: "Error Creating Post",
+        description: "There was an error creating your post",
+        status: "error",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    handleCreatePost,
+    loading,
+    error,
+  };
+};
+
+export default useCreatePost;
